19ed9d36d941934e02316cd782de0d818abb11d8
Michael Klein <mbklein@gmail.com>
2013-09-18 17:18:13 -0400

    Allow the driver to store CRSF data in the session during auth

--- app/controllers/browse_everything_controller.rb
+++ app/controllers/browse_everything_controller.rb
@@ -7,21 +7,25 @@ class BrowseEverythingController < ActionController::Base
   end
 
   def show
+    if @provider.present? and not @provider.authorized?
+      link, data = @provider.auth_link
+      @auth_link = "#{link}&state=#{@provider_name}"
+      session["#{@provider_name}_data"] = data
+    end
     render :layout => !request.xhr?
   end
   
   def load_browser
     @browser = BrowseEverything::Browser.new(url_options)
-    provider_name = params[:provider]
-    @provider = @browser.providers[provider_name]
-    @provider.token = session["#{provider_name}_token"] unless @provider.blank?
+    @provider_name = params[:provider] || params[:state].to_s.split(/\|/).last
+    @provider = @browser.providers[@provider_name]
+    @provider.token = session["#{@provider_name}_token"] unless @provider.blank?
     @path = params[:path] || ''
   end
 
   def auth
     code = params[:code]
-    provider_name = params[:state]
-    @provider = @browser.providers[provider_name]
-    session["#{provider_name}_token"] = @provider.connect(code)
+    @provider = @browser.providers[@provider_name]
+    session["#{@provider_name}_token"] = @provider.connect(params,session["#{@provider_name}_data"])
   end
 end
