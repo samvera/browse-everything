5d7bbf01f88e673605f14be9ec15581935c181e3
Chris Beer <chris@cbeer.info>
2017-08-11 15:26:32 -0700

    Add a single-repository github driver

---
+++ lib/browse_everything/driver/github.rb
@@ -0,0 +1,52 @@
+module BrowseEverything
+  module Driver
+    class Github < Base
+      require 'octokit'
+
+      def icon
+        'github'
+      end
+
+      def validate_config
+        unless config[:access_token]
+          raise BrowseEverything::InitializationError, 'Octokit driver requires a :access_token argument'
+        end
+        unless config[:repository]
+          raise BrowseEverything::InitializationError, 'Octokit driver requires a :repository argument'
+        end
+      end
+
+      def contents(path)
+        return to_enum(:contents, path) unless block_given?
+
+        client.contents(repo, path: path).each do |resource|
+          yield BrowseEverything::FileEntry.new(resource.path, [key, resource.path].join(':'), resource.name, resource.size, nil, resource.type == 'dir')
+        end
+      end
+
+      def details(path)
+        contents(path).first
+      end
+
+      def link_for(path)
+        resource = client.contents(repo, path: path)
+        [resource.download_url, { file_name: resource.name }]
+      end
+
+      def authorized?
+        true
+      end
+      
+      private
+      
+      def client
+        @client ||= ::Octokit::Client.new(access_token: config[:access_token])
+      end
+      
+      def repo
+        config[:repository]
+      end
+
+    end
+  end
+end
