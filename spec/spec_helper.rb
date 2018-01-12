f656338dfea6abcdf20da677a0b0157f3ef81b73
James R. Griffin III <jrgriffiniii@gmail.com>
2018-01-12 11:48:22 -0500

    Resolves #193 by restructuring and improving upon the Travis CI config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,22 @@ RSpec.configure do |config|
   config.include WaitForAjax, type: :feature
 end
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new(app,
+                                 browser: :chrome,
+                                 desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = :headless_chrome
+
 module BrowserConfigHelper
   def url_options
     {
