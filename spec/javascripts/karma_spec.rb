28c6f7229cdecb2723493fe163ecbf1ad59624b1
Michael B. Klein <mbklein@gmail.com>
2018-11-19 14:03:23 -0600

    Replace jasmine gem with jasmine via karma

---
+++ spec/javascripts/karma_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'open3'
+
+# Run karma and parse the output for failures.
+# The spec will fail if any karma test fails.
+describe 'Karma' do
+  let(:runner)  { Open3.capture3('karma', 'start') }
+  let(:output)  { runner[0] + runner[1] }
+  let(:status)  { runner[2].exitstatus }
+
+  it 'expects all karma tests to pass' do
+    $stderr.puts output unless status == 0
+    expect(status).to eq(0)
+  end
+end
