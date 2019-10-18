d702efdbd4f8b5141558bf0f83b6fdbaa8542c85
GriffinJ <jrgriffiniii@gmail.com>
2019-10-18 17:01:10 -0400

    Fixing the generator for RSwag bugs

--- browse-everything.gemspec
+++ browse-everything.gemspec
@@ -30,7 +30,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'jsonapi-resources'
   spec.add_dependency 'puma', '~> 3.11'
   spec.add_dependency 'rails', '>= 5.2' # We're going to need to require support for webpacker given the usage of rswag-ui to generate Swagger documentation
-  spec.add_dependency 'rswag'
+  spec.add_dependency 'rswag', '2.0.6' # Blocked until 2.1.z resolved https://github.com/rswag/rswag/issues/248
   spec.add_dependency 'ruby-box'
   spec.add_dependency 'signet', '~> 0.8'
   spec.add_dependency 'thor', '~> 0.19'
--- lib/generators/browse_everything/install_generator.rb
+++ lib/generators/browse_everything/install_generator.rb
@@ -48,6 +48,15 @@ class BrowseEverything::InstallGenerator < Rails::Generators::Base
       RUBY
   end
 
+  # Things get more complicated here with RSpec
+  # Need to install rspec, rspec-rails
+  def install_rspec
+    run 'rspec --init'
+    insert_into_file 'spec/spec_helper.rb', before: 'RSpec.configure do |config|' do
+      "\nrequire 'rspec'\nrequire 'rspec-rails'\n"
+    end
+  end
+
   def install_rswag
     # This is needed for a bug, as rswag will not install for the dependent app.
     # unless it is explicitly required here
@@ -56,31 +65,22 @@ class BrowseEverything::InstallGenerator < Rails::Generators::Base
     end
 
     generate 'rswag:install'
-  end
-
-  # Things get more complicated here with RSpec
-  # Need to install rspec, rspec-rails
-  def install_rspec
-    run 'rspec --init'
-    insert_into_file 'spec/spec_helper.rb', before: 'RSpec.configure do |config|' do
-      "\nrequire 'rspec'\n require 'rspec-rails'"
-    end
     gsub_file 'spec/swagger_helper.rb',
       "config.swagger_root = Rails.root.join('swagger').to_s",
       "rails_root_path = Pathname.new(File.dirname(__FILE__))\nconfig.swagger_root = rails_root_path.join('..', 'swagger').to_s"
   end
 
   def install_swagger_api_spec
-    FileUtils.mkdir_p 'swagger/v1'
+    FileUtils.mkdir_p Rails.root.join('swagger','v1')
     copy_file 'swagger/v1/swagger.json', 'swagger/v1/swagger.json'
   end
 
   def install_swagger_tests
-    FileUtils.mkdir_p 'spec/integration'
+    FileUtils.mkdir_p Rails.root.join('spec', 'integration')
     Dir.glob("spec/integration/*_spec.rb").each do |test_file_path|
       segments = test_file_path.split('/')
       target_segments = segments[1..]
-      target_path = target_segments.join('/')
+      target_path = Rails.root.join(*target_segments)
       copy_file test_file_path, target_path
     end
   end
