6c4b0440eebe121333ec836fa17ecb778c753498
GriffinJ <jrgriffiniii@gmail.com>
2019-10-21 11:05:37 -0500

    Fixing the provider serializer

--- app/serializers/provider_serializer.rb
+++ app/serializers/provider_serializer.rb
@@ -4,5 +4,7 @@ class ProviderSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name
 
-  link :authorization_url, &:authorization_url
+  link :authorization_url do |object|
+    object.authorization_url
+  end
 end
