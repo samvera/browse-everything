12fc3fa902f555d602abe4b70665518d3a124efc
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-04-15 16:19:52 -0400

    inject sprockets 3 requirement into local app Gemfile
    Now our installer -- which only works with sprockets 3 at present -- will still work. But the local app is free to change/erase this and upgrade to sprockets 4 itself. With the sprockets 3 requirement in our .gemspec, the local app was not

--- lib/generators/browse_everything/install_generator.rb
+++ lib/generators/browse_everything/install_generator.rb
@@ -10,4 +10,12 @@ class BrowseEverything::InstallGenerator < Rails::Generators::Base
   def inject_config
     generate 'browse_everything:config'
   end
+
+  # While there's no reason browse-everything can't work with a properly configured
+  # sprockets 4 (including configured to deal with Javascript), our install generator
+  # isn't capable of setting this up right now, so instead we'll inject into the app
+  # a sprockets 3 requirement...
+  def sprockets_3_restriction
+    gem 'sprockets', '~> 3.7'
+  end
 end
