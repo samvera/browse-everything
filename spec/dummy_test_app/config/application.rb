021e0c5edc88dd2789866013a60bb9bfb0c4b3f1
Jonathan Rochkind <jonathan@dnil.net>
2022-06-16 10:37:20 -0400

    test with defaults for currently running Rails version

--- spec/dummy_test_app/config/application.rb
+++ spec/dummy_test_app/config/application.rb
@@ -7,8 +7,9 @@ require "browse_everything"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # ~Initialize configuration defaults for originally generated Rails version.~
+    # Changed to: For currently running Rails version, eg 5.2 or 7.0:
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
